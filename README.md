08c42657e96ea67a9f51d4f5791a81bb6c8155eb
Tyler Long <tyler4long@gmail.com>
2022-01-26 10:07:17 -0800

    Two wsConnection cannot share the same wsToken

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # RingCentral WSG Multiple Connections Test
 
 Try to establish multiple WSG connections in an single process.
+
+Two WSG connections in a single process works: [./src/index.ts](./src/index.ts)
+
+But two WSG connections cannot share the same wsToken: [./src/index2.ts](./src/index2.ts).
---
+++ src/index2.ts
@@ -0,0 +1,68 @@
+import RingCentral from '@rc-ex/core';
+import WebSocketExtension from '@rc-ex/ws';
+import waitFor from 'wait-for-async';
+
+const rc1 = new RingCentral({
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+  server: process.env.RINGCENTRAL_SERVER_URL,
+});
+const webSocketExtension1 = new WebSocketExtension();
+
+const rc2 = new RingCentral({
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+  server: process.env.RINGCENTRAL_SERVER_URL,
+});
+
+const main = async () => {
+  await rc1.authorize({
+    username: process.env.RINGCENTRAL_USERNAME!,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD!,
+  });
+  await rc1.installExtension(webSocketExtension1);
+  await rc2.authorize({
+    username: process.env.RINGCENTRAL_USERNAME!,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD!,
+  });
+  const webSocketExtension2 = new WebSocketExtension({
+    wscToken: webSocketExtension1.wsToken.ws_access_token,
+  });
+  await rc2.installExtension(webSocketExtension2);
+  let eventCount1 = 0;
+  await webSocketExtension1.subscribe(
+    ['/restapi/v1.0/account/~/extension/~/message-store'],
+    () => {
+      console.log('event from first ws connection');
+      eventCount1 += 1;
+    }
+  );
+  let eventCount2 = 0;
+  await webSocketExtension2.subscribe(
+    ['/restapi/v1.0/account/~/extension/~/message-store'],
+    () => {
+      console.log('event from second ws connection');
+      eventCount2 += 1;
+    }
+  );
+  await rc1
+    .restapi()
+    .account()
+    .extension()
+    .companyPager()
+    .post({
+      from: {extensionNumber: '101'},
+      to: [{extensionNumber: '101'}], // send pager to oneself
+      text: 'Hello world',
+    });
+  await waitFor({
+    interval: 1000,
+    condition: () => eventCount1 >= 1 && eventCount2 >= 1,
+  });
+  await rc1.revoke();
+  await rc2.revoke();
+};
+
+main();
