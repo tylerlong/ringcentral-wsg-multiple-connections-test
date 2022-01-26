07fe8db520f7894efd7e60d2ef9150e3524fdd9f
Tyler Long <tyler4long@gmail.com>
2022-01-26 09:39:12 -0800

    Hello RingCentral

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello world!');
+import RingCentral from '@rc-ex/core';
+import WebSocketExtension from '@rc-ex/ws';
+
+const rc = new RingCentral({
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+  server: process.env.RINGCENTRAL_SERVER_URL,
+});
+
+const wsExtension = new WebSocketExtension();
+
+const main = async () => {
+  await rc.authorize({
+    username: process.env.RINGCENTRAL_USERNAME!,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD!,
+  });
+  await rc.installExtension(wsExtension);
+  const extInfo = await rc.restapi().account().extension().get();
+  console.log(extInfo.extensionNumber);
+  await rc.revoke();
+};
+
+main();
